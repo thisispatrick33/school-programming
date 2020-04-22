ba8a8c4125df6b26a1b74ec1ec8a6e81cd28ad76
Mamuss <matus.kuzma20@gmail.com>
2020-04-22 12:46:42 +0200

    4Games

---
+++ BreakOut/src/module-info.java
@@ -0,0 +1,12 @@
+module Pong {
+    requires javafx.media;
+    requires javafx.web;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.swt;
+
+    opens sample;
+}
